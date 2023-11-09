f7234f9a5d5ee523c71191cba76a82817530ef06
ShengBin-101 <shengbin.chan@gmail.com>
2023-11-09 09:26:20 +0800

    Added Launch File

--- src/camera/setup.py
+++ src/camera/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+from setuptools import setup
 from setuptools import find_packages, setup
 
 package_name = 'camera'
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
