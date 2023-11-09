efcc5bb5f95e9a4c98f35ea7a3b086fc54ba3599
ShengBin-101 <shengbin.chan@gmail.com>
2023-11-09 13:35:01 +0800

    Fixed Week11

--- src/camera/setup.py
+++ src/camera/setup.py
@@ -13,21 +13,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py'))
+        # Include all launch files. (includes both python and xml launch files)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.xml'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='shengbin',
-    maintainer_email='shengbin@todo.todo',
+    maintainer_email='shengbin.chan@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'cam_pub = camera.cam_pub:main',
-            'cam_sub = camera.cam_sub:main',
-            'cam_oflow = camera.cam_oflow:main'
         ],
     },
 )
