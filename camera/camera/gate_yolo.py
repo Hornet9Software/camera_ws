e302beb59f3db5faa657bd09aea89063013c7e53
ShengBin-101 <shengbin.chan@gmail.com>
2023-12-27 00:33:13 +0800

    yolo node to publish bounding box coordinates

--- camera/camera/gate_yolo.py
+++ camera/camera/gate_yolo.py
@@ -4,6 +4,7 @@ from sensor_msgs.msg import CompressedImage, Image
 from cv_bridge import CvBridge
 import cv2
 from ultralytics import YOLO
+from std_msgs.msg import Float32MultiArray
 
 ####################
 # This node is run in conjunction with the enhance node..
@@ -26,23 +27,45 @@ class GateDetectorNode(Node):
             self.image_callback,
             10
         )
-        self.publisher = self.create_publisher(
-            CompressedImage,
-            '/gate_detector_node/compressed',
+        # self.yolo_pub = self.create_publisher(
+        #     CompressedImage,
+        #     '/left/yolo/compressed',
+        #     10
+        # )
+        self.bbox_pub = self.create_publisher(
+            Float32MultiArray,
+            '/left/yolo/box',
             10
         )
         self.cv_bridge = CvBridge()
 
+
     def image_callback(self, msg):
         cv_image = self.cv_bridge.compressed_imgmsg_to_cv2(msg)
         
+        # detection
         results = model(cv_image)
+        
+        # get the bounding box
+        xyxy = results[0].boxes.xyxy.flatten().tolist()
+        # xyxy is now a list of 4 floats (x1, x2, y1, y2)
+
+        # terminate if no detection
+        if len(xyxy) == 0:
+            return
+
+        bbox_msg = Float32MultiArray()
+        bbox_msg.data = xyxy
+
+        # publish the message
+        self.bbox_pub.publish(bbox_msg)
+
 
         # Visualize the results on the frame
-        annotated_frame = results[0].plot()
-
-        compressed_msg = self.cv_bridge.cv2_to_compressed_imgmsg(annotated_frame)
-        self.publisher.publish(compressed_msg)
+        # annotated_frame = results[0].plot()
+        # compressed_msg = self.cv_bridge.cv2_to_compressed_imgmsg(annotated_frame)
+        # self.yolo_pub.publish(compressed_msg)
+        
         
 def main(args=None):
     rclpy.init(args=args)
