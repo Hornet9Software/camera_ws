ef277d0bc688fa3e8fd790c1539569a79b2e8791
ShengBin-101 <shengbin.chan@gmail.com>
2023-12-27 01:15:43 +0800

    rosbag record script for pooltest

--- record_scripts/detect_record.sh
+++ record_scripts/detect_record.sh
@@ -31,4 +31,4 @@ filename=$(printf "$filename_template" "$next_number")
 # /object/gate/distance
 # /object/gate/bearing
 
-ros2 bag record -o $filename /left/camera_info /right/camera_info /bottom/camera_info /left/image_raw /right/image_raw /bottom/image_raw /left/image_rect_color /right/image_rect/color /left/gray_world/compressed /gate/finalmask/compressed /object/gate/distance /object/gate/bearing
+ros2 bag record -o $filename /left/camera_info /right/camera_info /bottom/camera_info /left/image_raw /right/image_raw /bottom/image_raw
