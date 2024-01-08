73e20c4ea801da5ac8987d83d8bf6c4045bdf2d5
bb-jetson-2023 <bb.jetson.2023@gmail.com>
2024-01-08 21:24:47 +0800

    update calibration node

--- camera/camera/calibration.py
+++ camera/camera/calibration.py
@@ -4,14 +4,14 @@ from rclpy.node import Node
 from sensor_msgs.msg import CameraInfo
 
 # might need to edit the path name
-LEFT_CALIBRATION_PATH = "camera/calibration/calibrationdata/left.yaml"
-RIGHT_CALIBRATION_PATH = "camera/calibration/calibrationdata/right.yaml"
-BOTTOM_CALIBRATION_PATH = "camera/calibration/calibrationdata/bottom.yaml"
+LEFT_CALIBRATION_PATH = "src/camera_ws/camera/calibration/calibrationdata/left.yaml"
+RIGHT_CALIBRATION_PATH = "src/camera_ws/camera/calibration/calibrationdata/right.yaml"
+BOTTOM_CALIBRATION_PATH = "src/camera_ws/camera/calibration/calibrationdata/bottom.yaml"
 
 
 class CalibrationNode(Node):
     def __init__(self):
-        super.__init__("calibrator node")
+        super().__init__("calibrator_node")
         self.publisher_left = self.create_publisher(CameraInfo, "/left/calibration", 10)
         self.publisher_right = self.create_publisher(
             CameraInfo, "/right/calibration", 10
@@ -28,9 +28,9 @@ class CalibrationNode(Node):
         right_msg = self.generate_message(self.read_data(RIGHT_CALIBRATION_PATH))
         bottom_msg = self.generate_message(self.read_data(BOTTOM_CALIBRATION_PATH))
 
-        self.publisher_left.publish(self.generate_message(left_msg))
-        self.publisher_right.publish(self.generate_message(right_msg))
-        self.publisher_btm.publish(self.generate_message(bottom_msg))
+        self.publisher_left.publish(left_msg)
+        self.publisher_right.publish(right_msg)
+        self.publisher_btm.publish(bottom_msg)
 
         self.get_logger().info(f"left_calibration_data: {left_msg}")
         self.get_logger().info(f"right_calibration_data: {right_msg}")
@@ -38,50 +38,47 @@ class CalibrationNode(Node):
 
     def read_data(self, path):
         data = {
-            "name": None,
+            # "name": None,
             "height": None,
             "width": None,
-            "model": None,
-            "D": None,
-            "K": None,
-            "R": None,
-            "P": None,
+            "distortion_model": None,
+            "d": None,
+            "k": None,
+            "r": None,
+            "p": None,
         }
 
         with open(path, "r") as file:
             contents = yaml.safe_load(file)
-            data["name"] = contents["camera_name"]
-            data["height"] = contents["image_height"]
-            data["width"] = contents["image_width"]
-            data["model"] = contents["distortion_model"]
-            data["D"] = contents["distortion_coefficients"]["data"]
-            data["K"] = contents["camera_matrix"]["data"]
-            data["R"] = contents["rectification_matrix"]["data"]
-            data["P"] = contents["projection_matrix"]["data"]
-
+            # data["name"] = contents["camera_name"]
+            data["height"] = int(contents["image_height"])
+            data["width"] = int(contents["image_width"])
+            data["distortion_model"] = str(contents["distortion_model"])
+            data["d"] = list(contents["distortion_coefficients"]["data"])
+            data["k"] = list(contents["camera_matrix"]["data"])
+            data["r"] = list(contents["rectification_matrix"]["data"])
+            data["p"] = list(contents["projection_matrix"]["data"])
         return data
 
     def generate_message(self, data):
         calibration_msg = CameraInfo()
-
         # not sure if the time is right
         calibration_msg.header.stamp = self.get_clock().now().to_msg()
-        calibration_msg.height = data["height"]
-        calibration_msg.width = data["width"]
-        calibration_msg.distortion_model = data["model"]
-        calibration_msg.distortion_coefficients = data["D"]
-        calibration_msg.K = data["K"]
-        calibration_msg.R = data["R"]
-        calibration_msg.p = data["P"]
+        calibration_msg.height = data["height"] 
+        calibration_msg.width = data["width"] 
+        calibration_msg.distortion_model = data["distortion_model"]
+        calibration_msg.d = data["d"] 
+        calibration_msg.k = data["k"] 
+        calibration_msg.r = data["r"] 
+        calibration_msg.p = data["p"] 
 
         return calibration_msg
 
 
 def main(args=None):
     # reads the calibration data and publishes CameraInfo for other noddes to calibrate with
-    calibration_node = CalibrationNode()
-
     rclpy.init(args=args)
+    calibration_node = CalibrationNode()
 
     rclpy.spin(calibration_node)
     calibration_node.destroy_node()
